228f74975fb0a26b6884d5b6b3b125deb2c554c9
dbaba2011 <dbaba2011@yahoo.com>
2023-09-29 20:05:30 +0000

    update 0-pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """A script to determine pascal's triangle for any number"""
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     triangle = []
+
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
     for i in range(n):
-        row = [1] * (i + 1)
-        if i >= 2:
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
+        temp_list = []
 
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
     return triangle
-
-# Test the function with the given example
-if __name__ == "__main__":
-    triangle = pascal_triangle(5)
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
